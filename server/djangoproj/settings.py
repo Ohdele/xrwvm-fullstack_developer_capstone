ec1b7a4f90cbcb94cd44fdd3e4ba29e76ea9bd3a
Adesina Ayodele <adesinaayodele48@gmail.com>
2024-12-25 13:13:08 -0500

    Updated settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,19 +31,16 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    (
-        'adesinaayode-8000.theianext-1-labs-prod-misc-tools-us-east-0.'
-        'proxy.cognitiveclass.ai'
-    )
+    'adesinaayode-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.' + 
+    'proxy.cognitiveclass.ai'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000',
-    (
-        'https://adesinaayode-8000.theianext-1-labs-prod-misc-tools-us-east-0.'
-        'proxy.cognitiveclass.ai'
-    )
+    'https://adesinaayode-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.' + 
+    'proxy.cognitiveclass.ai'
 ]
+
 
 
 
